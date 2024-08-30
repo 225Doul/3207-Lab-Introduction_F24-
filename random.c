5930ad75085635c03426868cc30d438ca2cc26b9
225Doul <tuo68092@temple.edu>
2024-08-29 20:17:29 -0400

    finished creating the randchar() function and added it to both the header file and the main function (print_random.c)

---
+++ random.c
@@ -0,0 +1,11 @@
+//Abdoul Djedje lab1 08-29-2024
+#include "random.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+char randchar() {// This function returns a random character between 'A' and 'Z'
+    return 'A' + rand() % 26; // 'A' + random number between 0 and 25
+                                //(so it will add A to the random number generated which will = a random letter in the alphabet)
+}
