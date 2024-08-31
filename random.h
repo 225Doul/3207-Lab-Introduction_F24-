cfd35c2e971ef3f37f262fe953c978b56ee7d56a
225Doul <tuo68092@temple.edu>
2024-08-31 17:56:12 -0400

    updates made to file

--- random.h
+++ random.h
@@ -1,13 +1,11 @@
 //Abdoul Djedje lab1 08-29-2024 (header file which will link both random.c and print_random.c together)
-#ifndef string_h
-#define string_h
-#include <stddef.h> // For size_t because it gives me an error without this included statement 
+#ifndef random_h
+#define random_h
 
 
 
-// Function declarations
+
+// Function declaration
 char randchar();                   // Generates a random character
-void rand_string(char *s, size_t givenSize); // Generates a random string of given size
-char* rand_string_alloc(size_t size); // Allocates memory for a string and generates it
 
 #endif // random.h
